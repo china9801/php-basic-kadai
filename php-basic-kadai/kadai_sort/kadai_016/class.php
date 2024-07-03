d46e7789a64bfa6e2a494452833f6122556d8647
china9801 <x17f007@gmail.com>
2024-07-04 07:21:50 +0900

    kadai_016を再提出

--- php-basic-kadai/kadai_sort/kadai_016/class.php
+++ php-basic-kadai/kadai_sort/kadai_016/class.php
@@ -10,32 +10,38 @@
     <p>
         <?php
         class Food {
+          // プロパティを定義する
           private $name;
           private $price;
 
+          // コンストラクタを定義する
           public function __construct(string $name, int $price) {
             $this->name = $name;
             $this->price = $price;
           }
 
           public function show_price() {
-            echo $this->price;
+            echo $this->price . '<br>';
           }
         }
 
+        
         class Animal {
+          // プロパティを定義する
           private $name;
           private $height;
           private $weight;
 
+          // コンストラクタを定義する
           public function __construct(string $name, int $height, int $weight) {
             $this->name = $name;
             $this->height = $height;
             $this->weight = $weight;
           }
 
+          // height プロパティの値を出力するメソッド
           public function show_height() {
-            echo $this->height;
+            echo $this->height . '<br>';
           }
         }
 
@@ -43,7 +49,13 @@
         $dog = new animal('dog', 60, 5000);
 
         print_r($potato);
+        echo '<br>';
         print_r($dog);
+        echo '<br>';
+
+        $potato->show_price().'<br>';
+        $dog->show_height();
+
         ?>
     </p>
 </body>
